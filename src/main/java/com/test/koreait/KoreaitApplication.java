ce77a02428d43abaefb947a3c46c37634ea90a9b
ghdwhd0302 <ghdwhd0302@naver.com>
2022-12-20 09:35:22 +0900

    초기 버전

---
+++ src/main/java/com/test/koreait/KoreaitApplication.java
@@ -0,0 +1,13 @@
+package com.test.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreaitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/koreait/KoreaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.koreait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
